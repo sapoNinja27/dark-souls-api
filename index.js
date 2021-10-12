571181b8176d5531ad38063b5703ead7af92e25c
sapoNinja27 <jpronzanisantos@gmail.com>
2021-10-12 20:03:54 -0300

    ajuste port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 const routes = require('./routes')
 
 app.use(cors())
