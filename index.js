b1cbd441fadaf01ba51c12672b64fb7ff29cc1c9
sapoNinja27 <jpronzanisantos@gmail.com>
2021-10-12 19:54:42 -0300

    Adicionado cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.port || 8080
 const routes = require('./routes')
 
+app.use(cors())
 app.use('/api', routes)
 app.listen(port, function(){
     console.log('Server initiated')
